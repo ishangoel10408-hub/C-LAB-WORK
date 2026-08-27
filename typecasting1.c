93738c7e4181a5715969849fd8a38adc52c828d9
Ishan <ishangoel10408@gmail.com>
2026-08-27 13:02:19 +0530

    TYPECASTING

---
+++ typecasting1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[] = "2026";
+
+    int num = atoi(str);
+
+    printf("%d", num);
+
+    return 0;
+}
---
+++ typecasting2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    double result;
+
+    result = (double)a / b;
+
+    printf("%.2lf", result);
+
+    return 0;
+}
