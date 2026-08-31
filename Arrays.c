0323be1c4e9ddb0aec4741682c44b2eb97398225
Ishan <ishangoel10408@gmail.com>
2026-08-31 12:27:57 +0530

    Array Initialization

---
+++ Arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    printf("%d ", arr[0]);
+    printf("%d ", arr[1]);
+    printf("%d ", arr[2]);
+    printf("%d ", arr[3]);
+    printf("%d ", arr[4]);
+    printf("%d ", arr[5]);
+    printf("%d ", arr[6]);
+    printf("%d ", arr[7]);
+    printf("%d ", arr[8]);
+    printf("%d ", arr[9]);
+
+    return 0;
+}
